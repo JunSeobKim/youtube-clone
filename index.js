9e1c3dd9345bda58b65f809ddb44603732702353
MainDesktop_KimJunSeob <wnstjq616@gmail.com>
2019-09-07 02:14:33 +0900

    add middlewares

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-}
+
+const handleHome = (req, res) => res.send("Hello from home");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/profile", handleProfile);
+
+app.get("/", handleHome);
 
 app.listen(PORT, handleListening);
