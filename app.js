c12e50e0d13c2cf6d04446049c7122fb63447f12
MainDesktop_KimJunSeob <wnstjq616@gmail.com>
2019-09-10 23:17:24 +0900

    router

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
-
-const handleHome = (req, res) => res.send("Hello from home");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -17,10 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/profile", handleProfile);
-
-app.get("/", handleHome);
-
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
