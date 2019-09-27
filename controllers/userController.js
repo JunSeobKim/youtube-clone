d34b26f57a28a50aaa570a8edbe182f200621d26
MainDesktop_KimJunSeob <wnstjq616@gmail.com>
2019-09-27 23:24:29 +0900

    join

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,20 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if (password != password2) {
+    res.status(400);
+  } else {
+    // To Do: Register User
+    // To Do: Log user in
+    res.redirect(routes.home);
+  }
+  res.render("join", { pageTitle: "Join" });
+};
+
 export const login = (req, res) => res.render("login", { pageTitle: "Login" });
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Logout" });
