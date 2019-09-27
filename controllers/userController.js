b3f574054ebfb16c633c7a357eb1505d3e8ae2e4
MainDesktop_KimJunSeob <wnstjq616@gmail.com>
2019-09-28 00:56:28 +0900

    header

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+import routes from "../routes";
+
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
@@ -5,8 +7,9 @@ export const postJoin = (req, res) => {
   const {
     body: { name, email, password, password2 }
   } = req;
-  if (password != password2) {
+  if (password !== password2) {
     res.status(400);
+    res.render("join", { pageTitle: "Join" });
   } else {
     // To Do: Register User
     // To Do: Log user in
@@ -15,7 +18,12 @@ export const postJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
 
-export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Logout" });
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
