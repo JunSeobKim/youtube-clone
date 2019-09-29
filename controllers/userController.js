8191575c33cac11a633f87e5c657dc990f390981
MainDesktop_KimJunSeob <wnstjq616@gmail.com>
2019-09-29 23:55:13 +0900

    upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,8 +24,11 @@ export const postLogin = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const logout = (req, res) =>
-  res.render("logout", { pageTitle: "Logout" });
+export const logout = (req, res) => {
+  // To Do: Process Log Out
+  res.redirect(routes.home);
+};
+
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "UserDetail" });
