b0133f4851d624b780e980ffc24821ed49f91a40
MainDesktop_KimJunSeob <wnstjq616@gmail.com>
2019-10-01 23:05:46 +0900

    connect DB

--- db.js
+++ db.js
@@ -1,41 +1,15 @@
-export const videos = [
-  {
-    id: 11111,
-    title: "Video awesome",
-    description: "This is something I love",
-    views: 111,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 100000,
-      name: "Nicolas",
-      email: "nico@las.com"
-    }
-  },
-  {
-    id: 22222,
-    title: "Video awesome2",
-    description: "This is something I love",
-    views: 222,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 200000,
-      name: "Nicolas",
-      email: "nico@las.com"
-    }
-  },
-  {
-    id: 33333,
-    title: "Video awesome3",
-    description: "This is something I love",
-    views: 333,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 300000,
-      name: "Nicolas",
-      email: "nico@las.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB");
+const handleError = () => console.log(`X Error on DB Connection ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
